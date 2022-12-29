4277115a506fddd5507209ebcb3d468775d344a1
hugo Lancery <108919405+LanceryH@users.noreply.github.com>
2022-12-29 19:12:42 +0100

    Update module_reso.py

--- module_reso.py
+++ module_reso.py
@@ -61,7 +61,6 @@ def adaptive_rkf45(y, t0, tf, N, nb_corps, M):
         for j in range(0,nb_corps):
             Y[j*3:(j+1)*3,i]=yb[j*3:(j+1)*3,0] 
         y=yb    
-        print("etape:{} et pas:{}".format(i,h))    
     return Y
 
 
